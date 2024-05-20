fdd4af5614757936527f006dcc3cfd3eab4013f7
DarkWhiteOff <zeezou@MacBook-58-inch.localdomain>
2024-05-20 23:27:43 +0200

    5

--- ft_strstr.c
+++ ft_strstr.c
@@ -1,4 +1,16 @@
 char	*ft_strstr(char *str, char *tofind)
 {
-	
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (*tofind[i] == '\0')
+		return (str);
+	while (str[i] != '\0')
+	{
+		if (str[i] == tofind[j])
+
+		i++:
+	}
 }
